043369fad131635d48bd6bfbc7a7e95b2bb03455
nathan-lrbt <lherbette.nat@gmail.com>
2025-01-28 18:37:07 +0100

    tp8

--- tp4/main.c
+++ tp4/main.c
@@ -1,71 +1,93 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 /****************************/
-/* Vos fonctions ci-dessous */ 
+/* Vos fonctions ci-dessous */
 /****************************/
 
 // EXERCICE 2
 
-// EXERCICE 3
+struct Cell;
+struct Cell {
+  int val;
+  struct Cell *suivante;
+};
+typedef struct Cell cell;
+typedef struct {
+  cell *premiere;
+} liste;
 
+liste *creer_liste() {
+  liste *l = malloc(sizeof(liste));
+  l->premiere = 0;
+  return l;
+}
+
+void ajouter_element(liste *l, int x) {
+  cell *c;
+  c = malloc(sizeof(int));
+  c->val = x;
+  c->suivante = l->premiere;
+  l->premiere = c;
+}
+
+// EXERCICE 3
 
 /****************************/
 /* Vos fonctions ci-dessus **/
 /****************************/
 
 void print_list(int liste[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        printf("%d, ", *(liste + i));
-    }
-    printf("%d\n", *(liste + size - 1));
+  for (int i = 0; i < size - 1; i++) {
+    printf("%d, ", *(liste + i));
+  }
+  printf("%d\n", *(liste + size - 1));
 }
 
 void exercice1(void) {
   printf("\n\nEXERCICE 1\n\n");
 
-    /******************** Votre code ci-dessous ********************/
+  /******************** Votre code ci-dessous ********************/
   int n;
   printf("Entrer la taille n de la liste : ");
   scanf("%d", &n);
-  printf("%d\n", n);
 
-  int liste[n];
-  printf("%d\n", sizeof(liste)/sizeof(int));
-  for(int i = 0; i < n; i++){
-    liste[i] = i*i;
+  int *carre = malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    *(carre + i) = i * i;
   }
-  print_list(liste, n);
-    /******************** Votre code ci-dessus *********************/
 
-    return;
+  print_list(carre, n);
+  /******************** Votre code ci-dessus *********************/
+
+  return;
 }
 
 void exercice2(void) {
-    printf("\n\nEXERCICE 2\n\n");
+  printf("\n\nEXERCICE 2\n\n");
 
-    /******************** Votre code ci-dessous ********************/
+  /******************** Votre code ci-dessous ********************/
 
-    /******************** Votre code ci-dessus *********************/
+  /******************** Votre code ci-dessus *********************/
 
-    return;
+  return;
 }
 
 void exercice3(void) {
-    printf("\n\nEXERCICE 3\n\n");
+  printf("\n\nEXERCICE 3\n\n");
 
-    /******************** Votre code ci-dessous ********************/
+  /******************** Votre code ci-dessous ********************/
 
-    /******************** Votre code ci-dessus *********************/
+  /******************** Votre code ci-dessus *********************/
 
-    return;
+  return;
 }
 
-int main(void) { 
-    exercice1();
-    exercice2();
-    exercice3();
-    return 0;
+int main(void) {
+  exercice1();
+  exercice2();
+  exercice3();
+  return 0;
 }
-
--- tp8/main_empty.py
+++ tp8/main_empty.py
@@ -1,33 +1,38 @@
 from utils import exercice, GREEN, ENDC
 
 
-#************************************
-#***** Vos fonctions ci-dessous *****
-#************************************
+# ************************************
+# ***** Vos fonctions ci-dessous *****
+# ************************************
 
 # EXERCICE 1
+
 
 def hamming(str1, str2):
     # ******************** Votre code ci-dessous ********************
     return -1
     # ******************** Votre code ci-dessus *********************
 
+
 # EXERCICE 2
+
 
 def recherche_mot_force_brute(mot, texte):
     # ******************** Votre code ci-dessous ********************
     return -1
     # ******************** Votre code ci-dessus *********************
 
+
 # EXERCICE 3
 
-#***********************************
-#***** Vos fonctions ci-dessus *****
-#***********************************
+# ***********************************
+# ***** Vos fonctions ci-dessus *****
+# ***********************************
 
 #
 # EXERCICE 1
 #
+
 
 @exercice
 def exercice1():
@@ -35,9 +40,11 @@ def exercice1():
     str2 = input("str2 = ")
     print(f"\nHamming({str1}, {str2}) = {hamming(str1, str2)}.")
 
+
 #
 # EXERCICE 2
 #
+
 
 @exercice
 def exercice2():
@@ -47,10 +54,15 @@ def exercice2():
     if idx == -1:
         print(f"'{mot}' n'est pas dans '{texte}'")
     else:
-        print(f"'{mot}' trouvé à l'indice {idx} : {texte[:idx]}{GREEN}{texte[idx:idx+len(mot)]}{ENDC}{texte[idx+len(mot):]}")
+        print(
+            f"'{mot}' trouvé à l'indice {idx} : {texte[:idx]}{GREEN}{texte[idx:idx+len(mot)]}{ENDC}{texte[idx+len(mot):]}"
+        )
+
+
 #
 # EXERCICE 3
 #
+
 
 @exercice
 def exercice3():
